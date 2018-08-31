159a9c448bc804f10abd1279863152152ba3aacf
v10let <violet.wmoon@gmail.com>
2018-08-31 13:25:29 -0500

    I MADE A BANK... Im basically a Roman grain loaner

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,34 @@
 class Transfer
-  # your code here
+	attr_accessor :sender, :receiver, :amount
+	attr_reader :status, :balance
+
+	def initialize(sender, receiver, amount)
+		@sender = sender
+		@receiver = receiver
+		@amount = amount
+		@status = 'pending'
+	end
+
+	def valid?
+		sender.valid? && receiver.valid?
+	end
+
+	def execute_transaction
+		 if self.sender.balance > self.amount && @status != 'complete'
+			 self.sender.balance -= self.amount
+			 self.receiver.balance += self.amount
+			 @status = 'complete'
+		else
+			@status = 'rejected'
+			return "Transaction rejected. Please check your account balance."
+		end
+	end
+
+	def reverse_transfer
+		if @status == 'complete'
+			self.receiver.balance -= self.amount
+			self.sender.balance += self.amount
+			@status = 'reversed'
+		end
+	end
 end
